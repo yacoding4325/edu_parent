d5838658da339258f8e0710c3d2913df36714534
yxym0203 <1500081802@qq.com>
2022-07-31 21:48:14 +0800

    登录,获取用户信息

--- service/service_edu/src/main/java/com/yacoding/service/edu/controller/admin/LoginController.java
+++ service/service_edu/src/main/java/com/yacoding/service/edu/controller/admin/LoginController.java
@@ -1,10 +1,7 @@
 package com.yacoding.service.edu.controller.admin;
 
 import com.yacoding.service.base.result.R;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,8 +10,9 @@ import java.util.Map;
  * @Author yaCoding
  * @create 2022-07-30 下午 10:39
  */
+
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/admin/edu/user")
 @CrossOrigin
 public class LoginController {
 
@@ -22,11 +20,20 @@ public class LoginController {
      * 登录，获取到token
      */
     @PostMapping("/login")
-    public R getInfo() {
+    public R login(){
+        return R.ok().data("token","admin");
+    }
+
+
+    /**
+     * 获取用户信息
+     */
+    @GetMapping("/info")
+    public R getInfo(){
         Map<String,Object> map = new HashMap<>();
         map.put("roles","[admin]");
         map.put("name","admin");
-        map.put("avatar","[admin]");
+        map.put("avatar","https://javaclimb-file.oss-cn-beijing.aliyuncs.com/avatar/01.jpeg");
         return R.ok().data(map);
     }
 }
